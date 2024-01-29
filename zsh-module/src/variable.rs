0ee51d675536e80013caed6b1222f03206a2115c
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2024-01-28 21:59:16 -0600

    More changes to variable code

--- zsh-module/src/variable.rs
+++ zsh-module/src/variable.rs
@@ -1,6 +1,6 @@
 use crate::Zerror;
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     fmt, iter,
     sync::{
         mpsc::{SendError, Sender},
@@ -12,7 +12,7 @@ use std::{
 pub type VariableKey = String;
 
 /// The type we're sending to the internal mpsc channel
-pub type MpscVarType = (String, VarType);
+pub type MpscVarType = (VariableKey, VarType);
 
 /// WIP definition of a variable
 ///
@@ -21,12 +21,13 @@ pub type MpscVarType = (String, VarType);
 /// TODO: This is an owned value right now. Its existence should be tied directly to the referenced variable in zsh.
 #[derive(Debug)]
 pub struct Variable {
-    name: String,
+    name: VariableKey,
     // values may or may not be initialized. That's like running `typeset -a array` with no values.
     value: Option<VarType>,
-    pub read_only: bool,
-    pub scope: Visibility,
-    // pub mpsc_channel: Arc<Sender<MpscVarType>>,
+    /// All the special properties of this variable
+    flags: HashSet<TypeFlags>,
+    // TODO: This is an idea I had for thread-safe variable access. It may or may not be practical.
+    // mpsc_channel: Arc<Sender<MpscVarType>>,
 }
 impl Variable {
     /// This variable's name. Goes out of scope when the variable is dropped.
@@ -44,7 +45,10 @@ impl Variable {
     /// Update the value of this variable. This calls internal zsh functions
     ///
     /// TODO: Implement
-    pub fn set(&mut self, value: VarType) -> Result<(), Zerror> {
+    pub fn set(&mut self, value: VarType) -> Result<(), VarError> {
+        if self.flags.contains(&TypeFlags::ReadOnly) {
+            return Err(VarError::ValueSet(VarIntrospectionError::NotPermitted));
+        }
         // if let Err(e) = self.mpsc_channel.send((self.name, value)) {
         //     match e {
         //         SendError(_) => {
@@ -57,7 +61,12 @@ impl Variable {
         todo!();
         Ok(())
     }
-    // pub fn get(&self)
+    /// Get the current value of the variable from the environment, saving it in the internal cache that you can access with the `value` method.
+    ///
+    /// TODO: Implement, this might be redundant. There would likely be a time-accessed-to-time-updated problem if it used a cache.
+    pub fn refresh(&mut self) -> Result<(), VarError> {
+        todo!()
+    }
 }
 impl ZVariable for Variable {
     fn has_value(&self) -> bool {
@@ -83,17 +92,92 @@ impl ZVariable for Variable {
     }
 }
 
+/// The special variable type flags
+///
+/// You can print most of the flags of a zsh variable using `echo ${(t)variable}`
+///
+/// For further information, read the manpage `man zshexpn` or go to
+/// https://zsh.sourceforge.io/Doc/Release/Expansion.html#Parameter-Expansion
+///
+/// Example: The type of the `mapfile` array from `zsh/mapfile`
+///
+/// ```zsh
+/// print ${(t)mapfile}
+/// # association-hide-hideval-special
+/// ```
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum TypeFlags {
+    /// parameters that are local to the current function scope
+    Local,
+    /// for left justified parameters
+    ///
+    /// TODO: Learn what these are
+    LeftJustified,
+    /// for right justified parameters with leading blanks
+    ///
+    /// TODO: Learn what these are
+    RightBlanks,
+    /// Right-justified parameters with leading zeros
+    ///
+    /// TODO: Learn what these are
+    RightZeros,
+    /// parameters whose value is converted to all lower case when it is expanded
+    ///
+    /// TODO: Learn what these are
+    Lower,
+    /// parameters whose value is converted to all upper case when it is expanded
+    ///
+    /// TODO: Learn what these are
+    Upper,
+    /// Parameters that are read-only
+    ReadOnly,
+    /// Tagged parameters
+    ///
+    /// TODO: Learn what these are
+    Tag,
+    /// for parameters tied to another parameter in the manner of PATH (colon-separated list) and path (array),
+    /// whether these are special parameters or user-defined with `typeset -T`
+    ///
+    /// This property contains the key of the variable that it is tied to.
+    Tied(VariableKey),
+    /// Parameters exported to subprocesses
+    Export,
+    /// Arrays that only keep the first occurrence of a duplicate value
+    Unique,
+    /// Parameters that are hidden
+    Hide,
+    /// Parameters that have the `hideval` attribute
+    HideVal,
+    /// Parameters that are defined by the shell
+    Special,
+}
+
+/// Types of interaction with variables
+#[derive(Debug, Default, Clone, Copy)]
+pub enum InteractionType {
+    Set,
+    Unset,
+    Listen,
+    #[default]
+    Get,
+}
+
 /// A builder for variables, similar to the zsh builtin `typeset`
 ///
 /// Variable is meant to be a reference to a shell variable. This is meant to be owned and whatnot.
 #[derive(Debug)]
 pub struct VariableBuilder {
-    pub name: String,
+    pub name: VariableKey,
     pub value: Option<VarType>,
-    pub read_only: bool,
-    pub scope: Visibility,
+    pub flags: HashSet<TypeFlags>,
 }
 impl VariableBuilder {
+    /// Create a new variable
+    /// ```
+    /// let var: Variable = VariableBuilder::new("HOME").build()?;
+    /// assert_eq!(var.name(), "HOME");
+    /// assert_eq!(var.value().unwrap(), "/home/antonio");
+    /// ```
     pub fn new<S>(name: S) -> Self
     where
         S: AsRef<str>,
@@ -101,29 +185,55 @@ impl VariableBuilder {
         Self {
             name: name.as_ref().to_string(),
             value: None,
-            read_only: false,
-            scope: Visibility::Global,
+            flags: HashSet::new(),
         }
     }
-    pub fn readonly(&mut self) -> &mut Self {
-        self.read_only = true;
-        self
+    /// Try to add a property to this variable. If it is already present, this will do nothing.
+    ///
+    /// For tied variables, there is a special key called [`TypeFlags::Tied`] that you can manually specify.
+    /// If that variable does not exist already, it will be created and tied to this variable.
+    ///
+    /// If the variable exists already and is NOT tied to this variable, this will return an error.
+    ///
+    /// TODO: Find out if these are exclusive to certain variable types
+    pub fn add_property(&mut self, prop: TypeFlags) -> Result<&mut Self, VarError> {
+        // duplicate flag should only occur in this single case
+        // TODO: This uses clone() because TypeFlags::Tied has a String internally. Find some way to resolve this.
+        if let TypeFlags::Tied(key) = prop.clone() {
+            if self.flags.contains(&prop) {
+                return Err(VarTypesetError::TieDuplicateFlag(key).into());
+            }
+
+            // TODO: Check if they are actually tied or not
+            if VariableBuilder::new(&key).build().is_err() {
+                return Err(VarTypesetError::TieNotPermitted(key).into());
+            }
+        }
+
+        // we chillin
+        self.flags.insert(prop);
+        Ok(self)
     }
-    pub fn scope(&mut self, scope: Visibility) -> &mut Self {
-        self.scope = Visibility::Global;
-        self
-    }
+    /// Set the preliminary value of this variable.
     pub fn value(&mut self, value: VarType) -> &mut Self {
         self.value = Some(value);
         self
     }
-    pub fn build(self) -> Variable {
-        Variable {
+    /// Creates a [`Variable`] from this builder
+    pub fn build(self) -> Result<Variable, VarError> {
+        // might have set the preliminary cached value, so they probably didn't really want to query the value from the environment.
+        let has_no_value = self.value.is_none();
+
+        let mut out = Variable {
             name: self.name,
             value: self.value,
-            read_only: self.read_only,
-            scope: self.scope,
+            flags: self.flags,
+        };
+        if has_no_value {
+            out.refresh()?;
         }
+
+        Ok(out)
     }
 }
 
@@ -214,38 +324,25 @@ pub trait ZVariable {
     fn iter<'a>(&'a self) -> Box<dyn Iterator<Item = &'a Primitive> + 'a>;
 }
 
-/// The scope of a variable
-#[derive(Debug, Clone, Copy)]
-pub enum Visibility {
-    /// typeset -x
-    Export,
-    /// typeset -g
-    Global,
-    /// typest alone in a function, or local
-    Local,
-}
-
-/// Types of interaction with variables
-#[derive(Debug, Default, Clone, Copy)]
-pub enum InteractionType {
-    Set,
-    Unset,
-    Listen,
-    #[default]
-    Get,
-}
-
 /// Errors that could occur when trying to change a variable type at runtime
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
 pub enum VarTypesetError {
     ReadOnly,
+    /// Should only occur in the rare case that the HashMap contains a duplicate enum key+value
+    TieDuplicateFlag(VariableKey),
+    /// Failure to tie variables
+    TieNotPermitted(VariableKey),
+    InvalidType,
     Disallowed,
 }
 impl fmt::Display for VarTypesetError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            VarTypesetError::ReadOnly => write!(f, "Variable is read-only"),
-            VarTypesetError::Disallowed => write!(f, "Variable is disallowed"),
+            Self::ReadOnly => write!(f, "Variable is read-only"),
+            Self::TieDuplicateFlag(t) => write!(f, "Duplicate type flag provided: {:?}", t),
+            Self::TieNotPermitted(s) => write!(f, "Not permitted to tie variable: {}", s),
+            Self::InvalidType => write!(f, "Invalid variable type"),
+            Self::Disallowed => write!(f, "Variable is disallowed"),
         }
     }
 }
@@ -253,21 +350,24 @@ impl fmt::Display for VarTypesetError {
 /// Errors that could occur when interacting with the internal zsh variable
 ///
 /// TODO: Discover more
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
 pub enum VarIntrospectionError {
     /// Variable is invalid in some weird way that wouldn't happen while using the shell language
     InvalidVariable,
     /// When the zsh internal paramtab == realparamtab check fails
     MisalignedParamTab,
+    /// It is read-only or something
+    NotPermitted,
     /// The variable doesn't exist
     Nonexistent,
 }
 impl fmt::Display for VarIntrospectionError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            VarIntrospectionError::InvalidVariable => write!(f, "Invalid variable"),
-            VarIntrospectionError::MisalignedParamTab => write!(f, "Misaligned paramtab"),
-            VarIntrospectionError::Nonexistent => write!(f, "Variable doesn't exist"),
+            Self::InvalidVariable => write!(f, "Invalid variable"),
+            Self::MisalignedParamTab => write!(f, "Misaligned paramtab"),
+            Self::NotPermitted => write!(f, "Not permitted"),
+            Self::Nonexistent => write!(f, "Variable doesn't exist"),
         }
     }
 }
@@ -291,11 +391,11 @@ impl std::error::Error for VarError {}
 impl fmt::Display for VarError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            VarError::Typeset(e) => write!(f, "Error setting variable type: {}", e),
-            VarError::ValueSet(e) => write!(f, "Error setting variable value: {}", e),
-            VarError::ValueUnset(e) => write!(f, "Error unsetting variable: {}", e),
-            VarError::ValueGet(e) => write!(f, "Error getting variable value: {}", e),
-            VarError::Send(e) => write!(f, "Error sending to internal mpsc channel: {}", e),
+            Self::Typeset(e) => write!(f, "Error setting variable type: {}", e),
+            Self::ValueSet(e) => write!(f, "Error setting variable value: {}", e),
+            Self::ValueUnset(e) => write!(f, "Error unsetting variable: {}", e),
+            Self::ValueGet(e) => write!(f, "Error getting variable value: {}", e),
+            Self::Send(e) => write!(f, "Error sending to internal mpsc channel: {}", e),
         }
     }
 }
@@ -305,5 +405,10 @@ where
 {
     fn from(e: std::sync::mpsc::SendError<T>) -> Self {
         Self::Send(e.to_string())
+    }
+}
+impl From<VarTypesetError> for VarError {
+    fn from(e: VarTypesetError) -> Self {
+        Self::Typeset(e)
     }
 }
