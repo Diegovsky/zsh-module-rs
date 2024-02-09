135d77399284592de741de8e4fd446e8050a6768
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2024-02-08 20:03:20 -0600

    Finish param example

--- examples/parameters/src/lib.rs
+++ examples/parameters/src/lib.rs
@@ -1,25 +1,117 @@
-use zsh_module::{Module, ModuleBuilder, ZResult};
+use std::collections::HashMap;
+
+use zsh_module::{
+    zsh::{self, ParamValue},
+    CStrArray, MaybeZError, Module, ModuleBuilder, Opts, ZResult,
+};
 
 zsh_module::export_module!(parameters, setup);
 
 #[derive(Debug, Default)]
 pub struct ParameterModule {
-    pub seen: Vec<String>,
+    pub seen: HashMap<String, String>,
 }
 impl ParameterModule {
     #[inline]
     pub fn new() -> Self {
         Self::default()
     }
-    pub fn export_as_module(&self) -> ZResult<Module> {
+    pub fn export_as_module(self) -> ZResult<Module> {
         let module = ModuleBuilder::new(self)
-            .builtin(Self::greet_cmd, "greet_cmd".into())
+            .builtin(ParameterModule::get_param, "get_param".into())
+            .builtin(ParameterModule::get_param_short, "get_param_short".into())
             .build();
 
         Ok(module)
     }
-    fn greet_cmd(&mut self, _name: &str, _args: &[&str], _opts: Opts) -> MaybeError {
-        println!("Hello, world!");
+    /// A verbose example of how you can get the value of a parameter.
+    fn get_param(&mut self, _name: &std::ffi::CStr, args: CStrArray, _opts: Opts) -> MaybeZError {
+        for arg in args.iter_str().filter_map(|a| a.ok()) {
+            if let Some(mut p) = zsh::get(arg) {
+                let flags = p.flags();
+                // `p.get_value()` is a mutable function, so it must come after `p.flags()`
+                let value = p.get_value();
+
+                let cache_key = arg.to_string();
+
+                let cache_value = match value {
+                    ParamValue::Array(arr) => {
+                        let array_string = arr
+                            .iter()
+                            .map(|a| format!("\t'{}'", a.to_string_lossy().replace("'", "'\\''")))
+                            .collect::<Vec<_>>()
+                            .join("\n");
+
+                        println!(
+                            "'{}' is an array with the following elements:\n(\n{}\n)",
+                            arg, &array_string
+                        );
+
+                        array_string
+                    }
+                    ParamValue::Scalar(scal) => {
+                        // C strings don't technically have to be UTF-8, so we have to convert this.
+                        let s_string = scal.to_string_lossy().to_string();
+
+                        println!(
+                            "'{}' is a scalar (string) with the following value: '{}'",
+                            arg, &s_string
+                        );
+
+                        s_string
+                    }
+                    ParamValue::Float(float) => {
+                        println!(
+                            "'{}' is a floating-point number with the following value: {}",
+                            arg, float
+                        );
+
+                        // In this example, we only want Strings.
+                        float.to_string()
+                    }
+                    ParamValue::Integer(int) => {
+                        println!("'{}' is an integer with the following value: {}", arg, int);
+                        int.to_string()
+                    }
+                    ParamValue::HashTable => {
+                        println!("'{}' is a hash table. We don't support those yet.", arg);
+                        String::from("Hashtable (unsupported)")
+                    }
+                };
+                // cache the value if you want
+                self.seen.insert(cache_key, cache_value);
+            } else {
+                println!("Parameter not found, possibly uninitialized: {arg}");
+            }
+        }
         Ok(())
     }
+    /// A slightly shorter version
+    fn get_param_short(
+        &mut self,
+        _name: &std::ffi::CStr,
+        args: CStrArray,
+        _opts: Opts,
+    ) -> MaybeZError {
+        let params = args
+            .iter_str()
+            .filter_map(|a| a.ok())
+            .filter_map(|a| {
+                if let Some(mut v) = zsh::get(a) {
+                    Some(format!("{}: {:?}", a, v.get_value()))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        println!("{}", params);
+
+        Ok(())
+    }
+}
+
+fn setup() -> ZResult<Module> {
+    ParameterModule::new().export_as_module()
 }
